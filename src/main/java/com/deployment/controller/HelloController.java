190aff6fd459c1e9da0a39c2f014e52a9794c709
unknown <aminessbaa@gmail.com>
2025-08-30 19:38:35 +0200

    third  commit

--- src/main/java/com/deployment/controller/HelloController.java
+++
@@ -1,9 +0,0 @@
-package com.deployment.controller;
-
-@RestController
-public class HelloController {
-    @GetMapping("/amin")
-    public String hello() {
-        return "Hello World!";
-    }
-}
--- src/main/java/com/deployment/demo/DemoApplication.java
+++ src/main/java/com/deployment/demo/DemoApplication.java
@@ -3,11 +3,17 @@ package com.deployment.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
